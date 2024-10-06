87b019b4f8eb5520b13c99e5daf4d4aa691f0929
Pedro Gonçalves <pedrogoncalves.ie@gmail.com>
2024-10-06 13:56:34 -0300

    Readme.md update

--- src/routes/news.js
+++ src/routes/news.js
@@ -5,19 +5,15 @@ const { fetchNews } = require("../controllers/newsController");
 router.get("/", async (req, res) => {
   const topHeadlines = await fetchNews();
   const sportsNews = await fetchNews("sports", 5);
-  const cookingNews = await fetchNews("gastronomy", 5);
   const techNews = await fetchNews("technology", 5);
   const financeNews = await fetchNews("business", 5);
-  const entertainmentNews = await fetchNews("entertainment", 5);
 
 
   res.render("index", {
     topHeadlines,
     sportsNews,
-    cookingNews,
     techNews,
     financeNews,
-    entertainmentNews,
   });
 });
 
